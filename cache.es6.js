6b0493b9f2316275b685e5e9907fcc88a5f6545c
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-08-29 15:32:24 +0200

    change internal caching from Object to Map

--- cache.es6.js
+++ cache.es6.js
@@ -13,25 +13,37 @@ function work(...args) {
 	return args.reduce((a, b) => a + Math.random() * b, Math.random());
 }
 function makeCaching(f) {
-	let cache = {};
+	const cache = new Map();
 
-	decorator.flush = () => {
-		forEach(cache, v => {
-			v = undefined;
-		}); // explicit set values = null for complicated environment GC
-		cache = {}; // create a new object
-	};
+	/*cache.get = seekKey => {
+ 	let currentKey
+ 	return some(cache.keys(), cacheKey => {
+ 		currentKey = cacheKey
+ 		return seekKey === cacheKey
+ 	}) ? currentKey : null
+ }*/
+
+	decorator.flush = cache.clear.bind(cache); /*() => {
+                                            forEach(cache, v => { v = undefined } ) // explicit set values = null for complicated environment GC
+                                            cache = {} // create a new object
+                                            }*/
+
 	function decorator(...args) {
 		const key = args.join("@");
-		return cache[key] ? cache[key] : cache[key] = f(...args), cache[key];
+		return cache.has(key) ? cache.get(key) : cache.set(key, f(...args)).get(key);
 	}
 	return decorator;
 }
 // No modifications of work are allowed. Your code should reside only in makeCaching.
-
-function forEach(collection, f) {
-	for (let item in collection) f(item);
+function some(collection, f) {
+	for (let item of collection) if (f(item)) return true;
+	return false;
 }
+
+/*function forEach(collection, f) {
+	for(let item of collection)
+		f(item)
+}*/
 
 exports.makeCaching = makeCaching;
 exports.work = work;
