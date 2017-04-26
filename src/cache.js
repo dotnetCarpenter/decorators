ffb8e056ac5148583797052375f2b45e2d6ba999
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-04-26 15:38:02 +0200

    serialize objects in cache so graphs can be cached and not only primitive values

--- src/cache.js
+++ src/cache.js
@@ -1,8 +1,10 @@
-/*Create a function makeCaching(f) which takes a one-argument function f(arg), and makes a wrapper over it which caches calls.
-
+/*
+Create a function makeCaching(f) which takes a one-argument function f(arg),
+and makes a wrapper over it which caches calls.
 The wrapper should have a static flush() method to flush the cache.
-
 Function f is allowed to have only one argument.
+Since we have the spread operator we accept multiple arguments
+as an array - hence it's still a single argument.
 */
 "use strict"
 
@@ -12,7 +14,7 @@ function makeCaching(f) {
 	decorator.flush = cache.clear.bind(cache)
 
 	function decorator(...args) {
-		const key = args.join("@")
+		const key = JSON.stringify(args)
 		return cache.has(key) ?
 			cache.get(key) :
 				cache.set(key, f(...args))
--- test/cache.js
+++ test/cache.js
@@ -4,6 +4,8 @@ import * as tap from "tap"
 import makeCaching from "../src/cache.es5"
 
 function work(...args) { return args.reduce((a,b) => a + Math.random()*b, Math.random()) }
+function workWithObject(obj) { return work.apply(null, Object.values(obj)) }
+
 
 tap.test("simple caching", (t) => {
 	t.plan(2)
@@ -12,10 +14,10 @@ tap.test("simple caching", (t) => {
 	let a = memWork(1)
 	let b = memWork(1)
 
-	t.ok(a == b, "a == b should be true (cached)") // true (cached)
+	t.equal(a, b, "a === b should be true (cached)")
 	memWork.flush()    // clears the cache
 	b = memWork(1)
-	t.not(a == b, "a == b should be false") // false
+	t.notEqual(a, b, "a === b should be false")
 })
 
 tap.test("dyadic caching", (t) => {
@@ -25,14 +27,14 @@ tap.test("dyadic caching", (t) => {
 	let a = memWork(2, 4)
 	let b = memWork(2, 4)
 
-	t.ok(a == b, "a == b should be true (cached)") // true (cached)
+	t.equal(a, b, "a === b should be true (cached)")
 	
 	b = memWork(2, 4.2)
-	t.not(a == b, "a == b should be false")
+	t.notEqual(a, b, "a === b should be false")
 
 	memWork.flush()    // clears the cache
 	b = memWork(2, 4)
-	t.not(a == b, "a == b should be false") // false
+	t.notEqual(a, b, "a === b should be false")
 })
 
 tap.test("triadic caching", (t) => {
@@ -42,12 +44,35 @@ tap.test("triadic caching", (t) => {
 	let a = memWork(5, 7, 2)
 	let b = memWork(5, 7, 2)
 
-	t.ok(a == b, "a == b should be true (cached)") // true (cached)
+	t.equal(a, b, "a === b should be true (cached)")
 	
 	b = memWork(1, 4.2, 41)
-	t.not(a == b, "a == b should be false")
+	t.notEqual(a, b, "a === b should be false")
 
 	memWork.flush()    // clears the cache
 	b = memWork(1, 4.2, 41)
-	t.not(a == b, "a == b should be false") // false
+	t.notEqual(a, b, "a === b should be false")
 })
+
+tap.test("object caching", (t) => {
+	t.plan(4)
+
+	const memWork = makeCaching(workWithObject)
+	let a = memWork({a:1})
+	let b = memWork({a:1})
+
+	t.equal(a, b, "a === b should be true (cached)")
+	
+	b = memWork({b:1})
+	t.notEqual(a, b, "a === b should be false")
+
+	memWork.flush()	// clears the cache
+	b = memWork({a:1})
+	t.notEqual(a, b, "a === b should be false")
+
+	memWork.flush()	// clears the cache
+	a = memWork({a:3,b:2,c:5,d:-1})
+	b = memWork({a:3,b:2,c:5,d:-1})
+	t.equal(a, b, "a === b should be true (cached)")
+})
+
