4ad54afc01e84f6ab03370e21b5691e41b072692
dotnetCarpenter <jon.ronnenberg@gmail.com>
2021-01-24 16:10:56 +0100

    fixed another typo - variable name

--- test/guard.js
+++ test/guard.js
@@ -3,17 +3,17 @@
 import tap from "tap"
 import {guard, guards} from "../src/guard.es5"
 
-function devide(a, b) {
+function divide(a, b) {
 	return a/b
 }
 
 tap.test("guard for zero", t => {
 	t.plan(2)
 
-	const guardedDevide = guard(devide, guards.zero(1))
+	const guardedDivide = guard(divide, guards.zero(1))
 
-	t.notThrow(guardedDevide.bind(null, 5, 1), "should not throw with an input of 5,1")
-	t.throw(guardedDevide.bind(null, 5, 0), new TypeError(), "should throw with an input of 5,0")
+	t.notThrow(guardedDivide.bind(null, 5, 1), "should not throw with an input of 5,1")
+	t.throw(guardedDivide.bind(null, 5, 0), new TypeError(), "should throw with an input of 5,0")
 })
 
 tap.test("guard for zero with custom error message", t => {
@@ -21,7 +21,7 @@ tap.test("guard for zero with custom error message", t => {
 
 	const expected = new TypeError("Don't call devide with zero")
 
-	const guardedDevide = guard(devide, guards.zero(1, expected))
+	const guardedDivide = guard(divide, guards.zero(1, expected))
 
-	t.throw(guardedDevide.bind(null, 5, 0), expected, "should throw a custom error")
+	t.throw(guardedDivide.bind(null, 5, 0), expected, "should throw a custom error")
 })
